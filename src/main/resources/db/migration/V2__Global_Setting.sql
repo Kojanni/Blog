5fc80db535458724d16e8787036bad9c81f13284
KEA <kjanni22@gmail.com>
2020-07-20 19:41:04 +0300

    added migration  for global setting data

---
+++ src/main/resources/db/migration/V2__Global_Setting.sql
@@ -0,0 +1,19 @@
+INSERT IGNORE INTO `global_settings`
+(`id`,
+`code`,
+`name`,
+`value`)
+VALUES
+(1,
+'MULTIUSER_MODE',
+'Многопользовательсикй режим',
+'YES'),
+(2,
+'POST_PREMODERATION',
+'Премодерация постов',
+'YES'),
+(3,
+'STATISTICS_IS_PUBLIC',
+'Показывать всем статистику',
+'NO');
+
